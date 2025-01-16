8ddfdd6185cd55a15a1500f42a3fac35d9e122a2
ToniXWD <807077266@qq.com>
2025-01-16 18:59:45 +0800

    finish load model

--- src/params.rs
+++ src/params.rs
@@ -22,14 +22,40 @@ pub struct LLamaParams<T> {
 
 impl LLamaParams<f32> {
     pub fn from_safetensors(safetensor: &SafeTensors, config: &LlamaConfigJson) -> Self {
-        todo!("实现从safetensors文件的模型参数加载");
-        // let get_tensor: impl Fn(&str) -> Tensor<f32> = |name: &str| {
-        // ...    
-        // };
-        
-        // LLamaParams {
-        //     embedding_table: get_tensor(...),
-        //     ...
-        // }
+        let get_tensor = |name: &str| {
+            let tensor = safetensor.tensor(name).unwrap();
+            let data_ptr = tensor.data().as_ptr() as *const f32;
+            let data_f32 =
+                unsafe { std::slice::from_raw_parts(data_ptr, tensor.shape().iter().product()) }
+                    .to_vec();
+            let shape: Vec<usize> = tensor.shape().to_vec();
+            Tensor::new(data_f32, &shape)
+        };
+
+        let layer_num = config.num_hidden_layers as usize;
+
+        let get_tensors = |name: &str| {
+            let mut res: Vec<Tensor<f32>> = vec![];
+            for i in 0..layer_num {
+                let name = format!("model.layers.{i}.{name}");
+                res.push(get_tensor(&name));
+            }
+            res
+        };
+
+        LLamaParams {
+            embedding_table: get_tensor("lm_head.weight"),
+            rms_att_w: get_tensors("input_layernorm.weight"),
+            wq: get_tensors("self_attn.q_proj.weight"),
+            wk: get_tensors("self_attn.k_proj.weight"),
+            wv: get_tensors("self_attn.v_proj.weight"),
+            wo: get_tensors("self_attn.o_proj.weight"),
+            rms_ffn_w: get_tensors("post_attention_layernorm.weight"),
+            w_up: get_tensors("mlp.up_proj.weight"),
+            w_gate: get_tensors("mlp.gate_proj.weight"),
+            w_down: get_tensors("mlp.down_proj.weight"),
+            rms_out_w: get_tensor("model.norm.weight"),
+            lm_head: get_tensor("lm_head.weight"),
+        }
     }
 }
